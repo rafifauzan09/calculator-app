2ea6f2aae2f5a7cabc3c4702698096042d8640f2
rafifauzan09 <rafif.almahdy@students.amikom.ac.id>
2020-03-10 12:10:24 +0700

    Halo

--- Calculator.cs
+++ Calculator.cs
@@ -1,7 +1,23 @@
 class Calculator
 {
-	static void Main(string[] args)
+   static void Main(string[] arg)
+    {
+	   int a = 10;
+	   int b = 6;
+	   
+	   Console.WriteLine ("Hasil Penambahan : {0} + {1} = {2}", a, b, Penambahan(a,b));
+	   Console.WriteLine ("Hasil Pengurangan : {0} - {1} = {2}", a, b, Pengurangan(a,b));
+	   
+	   Console.WriteLine ("\nTekan sembarang key untuk keluar");
+	   Console.ReadKey();
+    }
+	static int Penambahan(int a, int b)
 	{
-		
+		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
